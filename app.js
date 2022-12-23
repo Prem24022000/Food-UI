18f4d81d610aa76a36a928e3bf0b3b5e5daaab37
Prem Prakash Sahu <69319430+Prem24022000@users.noreply.github.com>
2022-12-23 12:59:38 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const nav = document.querySelector("nav");
 
+const navList = document.querySelector("nav ul");
+
+const navLinks = document.querySelectorAll("li a");
+
 let yAxis;
 
 function addMove() {
@@ -19,3 +23,47 @@ function addMove() {
 addMove();
 
 window.removeEventListener("scroll", addMove);
+
+if (window.innerWidth <= 426) {
+  const img = document.createElement("img");
+
+  img.classList = "menu-img";
+
+  img.setAttribute("src", "Images/menu.png");
+
+  img.setAttribute("alt", "menu");
+
+  nav.appendChild(img);
+
+  nav.style.background = "red";
+
+  img.onclick = function () {
+    if (img.alt === "menu") {
+      img.setAttribute("src", "Images/close.png");
+
+      img.setAttribute("alt", "close");
+
+      navList.style.display = "block";
+
+      nav.style.height = "100%";
+
+      nav.style.flexDirection = "column";
+
+      navLinks.forEach((navLink) => {
+        navLink.style.color = "white";
+      });
+
+      console.log(img);
+    } else {
+      img.setAttribute("src", "Images/menu.png");
+
+      img.setAttribute("alt", "menu");
+
+      navList.style.display = "none";
+
+      nav.style.height = "16%";
+
+      nav.style.flexDirection = "row";
+    }
+  };
+}
